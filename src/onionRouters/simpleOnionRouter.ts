19781f922927a0dd317c4ac845ab8c1d24f73a1e
alexiavinci <alexia.igue@edu.devinci.fr>
2024-03-26 17:25:35 +0100

    td4

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,6 +1,9 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_ONION_ROUTER_PORT } from "../config";
+
+import { BASE_ONION_ROUTER_PORT, REGISTRY_PORT,BASE_USER_PORT } from "../config";
+import { generateRsaKeyPair, exportPubKey, exportPrvKey } from "../crypto";
+import { rsaDecrypt, symDecrypt, importPrvKey, importSymKey } from "../crypto";
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
@@ -9,6 +12,72 @@ export async function simpleOnionRouter(nodeId: number) {
 
   // TODO implement the status route
   // onionRouter.get("/status", (req, res) => {});
+
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: number | null = null;
+
+  const { publicKey, privateKey } = await generateRsaKeyPair();
+  const privateKeyBase64 = await exportPrvKey(privateKey);
+  const publicKeyBase64 = await exportPubKey(publicKey);
+
+  const registryResponse = await fetch(
+    `http://localhost:${REGISTRY_PORT}/registerNode`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nodeId: nodeId,
+        pubKey: publicKeyBase64,
+      }),
+    }
+  );
+
+  if (!registryResponse.ok) {
+    throw new Error(`Failed to register node: ${registryResponse.statusText}`);
+  }
+
+  onionRouter.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
+
+  onionRouter.get("/getPrivateKey", (req, res) => {
+    res.json({ result: privateKeyBase64 });
+  });
+
+  onionRouter.post("/message", async (req, res) => {
+    const layer = req.body.message;
+    const encryptedSymKey = layer.slice(0, 344);
+    const symKey = privateKeyBase64 ? await rsaDecrypt(encryptedSymKey, await importPrvKey(privateKeyBase64)) : null;
+    const encryptedMessage = layer.slice(344) as string;
+    const message = symKey ? await symDecrypt(symKey, encryptedMessage) : null;
+    lastReceivedEncryptedMessage = layer;
+    lastReceivedDecryptedMessage = message ? message.slice(10) : null;
+    lastMessageDestination = message ? parseInt(message.slice(0, 10), 10) : null;
+    await fetch(`http://localhost:${lastMessageDestination}/message`, {
+      method: "POST",
+      body: JSON.stringify({ message: lastReceivedDecryptedMessage }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res.send("success");
+  });
+
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
@@ -20,3 +89,9 @@ export async function simpleOnionRouter(nodeId: number) {
 
   return server;
 }
+
+
+
+
+
+
--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -20,7 +20,27 @@ export async function launchRegistry() {
 
   // TODO implement the status route
   // _registry.get("/status", (req, res) => {});
+  let getNodeRegistryBody: GetNodeRegistryBody = { nodes: [] };
 
+  _registry.post("/registerNode", (req, res) => {
+    const { nodeId, pubKey } = req.body;
+    if (getNodeRegistryBody.nodes.some(n => n.nodeId === nodeId || n.pubKey === pubKey)) {
+      return res.send("Node already registered or public key in use.");
+    }else{
+      getNodeRegistryBody.nodes.push({ nodeId, pubKey });
+      return res.send("Node registered successfully.");
+    }
+  });
+  
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.json(getNodeRegistryBody);
+  });
+
+  _registry.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
   });
